6115cccafca0d74b1c1c562c923136644b5f2063
nenyeonyema <chinenyeonyema1@gmail.com>
2023-07-13 12:59:20 -0700

    Used printf to print strings of characters

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,14 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
+ * main - using the printf function to print strings of characters
  *
+ * description: use only the printf function and not puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
