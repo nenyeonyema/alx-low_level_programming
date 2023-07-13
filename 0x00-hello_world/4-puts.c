9cb21b99d8dda9e3d510db27221de7de2ce07c49
nenyeonyema <chinenyeonyema1@gmail.com>
2023-07-13 11:56:38 -0700

    Puts strings of characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main: Prints strings of characters using puts function
+ * main - prints strings of characters using puts function
  *
  * description: write a C programe that puts out strings of characters
  *
- * Return: (0)
+ * Return: 0
  */
 
 int main(void)
