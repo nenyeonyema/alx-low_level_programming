bc5bb2b3dd15478d2a2eb7ff859b292e43533e61
nenyeonyema <chinenyeonyema1@gmail.com>
2023-07-23 15:41:47 -0700

    Addition of multiples of 3 and 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,15 +3,15 @@
 /**
  * main - List all natural numbers below 1024
  *
- * Return: Always 0
+ * Return: void
  */
 
 int main(void)
 {
-	int limit = 1024;
 	int sum = 0;
+	int i;
 
-	for (int i = 0; i < limit; i++)
+	for (int i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
@@ -19,6 +19,5 @@ int main(void)
 		}
 	}
 	printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
-	return (0);
 }
 
