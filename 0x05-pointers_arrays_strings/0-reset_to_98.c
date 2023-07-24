51a371c31080a89090b7f57db459e54cca80d23f
nenyeonyema <chinenyeonyema1@gmail.com>
2023-07-24 14:35:01 -0700

    Changes the value of a variable through a pointer by deferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function deference the value on
+ * through its pointer
+ * @n: function parameter and pointer
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
