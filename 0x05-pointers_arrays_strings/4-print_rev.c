353ba72e8c79ad2adde5ecaa17afc5a10b8484de
nenyeonyema <chinenyeonyema1@gmail.com>
2023-07-24 18:37:56 -0700

    prints reverse of string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,12 @@
 void print_rev(char *s)
 {
 	int i;
-	int j = 0;
+	int count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		j++;
-	}
-	for (i = j - 1; s[i] >= 0; i--)
-	{
+		count++;
+	for (i = count - 1; i  >= 0; i--)
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 
 }
