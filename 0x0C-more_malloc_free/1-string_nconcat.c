d4d94d32308dbd83ec904727a1ac220d06ee5072
nenyeonyema <chinenyeonyema1@gmail.com>
2023-08-11 15:36:18 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* string_nconcat - a function that concatenates two strings
-* @s1: first arrary of strings pointer
-* @s2: second arrary of strings pointer
-* @n: unsigned int
-* Return: a char pointer
-*/
-
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, k, m = 0;
-char *str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-for (i = 0; s1[i] != '\0'; i++)
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-for (j = 0; s2[j] != '\0'; j++)
+	if (!s)
+		return (NULL);
 
-if (str == NULL)
-return (NULL);
-if (n < j)
-str = malloc((i + n + 1) * sizeof(char));
-else
-str = malloc((i + j + 1) * sizeof(char));
-if (str == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-str[k] = s1[k];
-for (n < j && k < (i + n))
-str[k++] = s2[m++];
-for (n >= j && k < (i + j))
-str[k++] = s2[m++];
-strr[k] = '\0';
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-return (str);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
