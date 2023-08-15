c25ddbd4f1e9fa57d6ff945683ecdd3a942ca561
nenyeonyema <chinenyeonyema1@gmail.com>
2023-08-15 10:20:39 -0700

    Define a data type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: char object
+ * @age: float object
+ * @owner: char object
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif /* DOG_H */
