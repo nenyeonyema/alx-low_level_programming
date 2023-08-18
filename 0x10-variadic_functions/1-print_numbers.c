b03154bd255d572bfdaad2fc5066e7775f77b723
nenyeonyema <chinenyeonyema1@gmail.com>
2023-08-17 17:43:58 -0700

    a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,6 +5,7 @@
  * print_numbers -  a function that prints numbers, followed by a new line
  * @separator: a string
  * @n: number of arguments
+ * @...: A variable number of numbers to be printed.
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
