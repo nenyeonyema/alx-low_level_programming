2433e5aaa2126e7f8facffef849e7faf31838784
nenyeonyema <chinenyeonyema1@gmail.com>
2023-08-30 14:06:22 -0700

    a function that inserts a new ode at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,12 +23,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = next->node;
+		return (new_node);
+	}
 	ptr = *head;
-	while (idx > 1)
+	while (ptr != NULL && idx > 1)
 	{
 		ptr = ptr->next;
 		idx--;
 	}
+
+	if (ptr == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = ptr->next;
 	ptr->next = new_node;
 
