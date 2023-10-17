c3a38c4083f51986ebc8c8508cbd3d3f8e2e7d9c
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-16 17:38:18 -0700

    random number jackpot

---
+++ 0x18-dynamic_libraries/randomnum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jackpot_random_number - a function generates a random
+ * number using the rand() function.
+ *
+ * Return: An integer representing the generated random number.
+ */
+int jackpot_random_number()
+{
+	return rand();
+}
