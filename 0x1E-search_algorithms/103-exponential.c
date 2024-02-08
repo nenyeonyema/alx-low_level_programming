bc7a53d62d50cc45cd0a2b97af2cc13194a9b27f
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-08 15:35:29 -0800

    binary search

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+* exponential_search - Searches for a value in a sorted array of integers
+*                       using the Exponential search algorithm.
+*
+* @array: A pointer to the first element of the array to search in.
+* @size: The number of elements in the array.
+* @value: The value to search for.
+*
+* Return: The index where value is located, or -1 if not found or array is NULL.
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+size_t bound = 1, low, high;
+
+if (array == NULL)
+return (-1);
+
+while (bound < size && array[bound] < value)
+{
+printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+bound *= 2;
+}
+
+low = bound / 2;
+high = (bound < size - 1) ? bound : size - 1;
+
+printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+return (binary_search(array, size, value, low, high));
+}
+
+/**
+* binary_search - Searches for a value in a sorted array of integers
+*                 using the Binary search algorithm.
+*
+* @array: A pointer to the first element of the array to search in.
+* @size: The number of elements in the array.
+* @value: The value to search for.
+* @low: The lower bound of the array to search.
+* @high: The upper bound of the array to search.
+*
+* Return: The index where value is located, or -1 if not found.
+*/
+int binary_search(int *array, size_t size, int value, size_t low, size_t high)
+{
+size_t mid;
+
+while (low <= high)
+{
+size_t i;
+
+printf("Searching in array: ");
+for (i = low; i < high; i++)
+printf("%d, ", array[i]);
+printf("%d\n", array[i]);
+
+mid = (low + high) / 2;
+
+if (array[mid] == value)
+return (mid);
+
+if (array[mid] < value)
+low = mid + 1;
+else
+high = mid - 1;
+}
+
+return (-1);
+}
---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+* advanced_binary - Searches for a value in a sorted array of integers.
+*
+* @array: A pointer to the first element of the array to search in.
+* @size: The number of elements in the array.
+* @value: The value to search for.
+*
+* Return: The index where value is located, or -1 if not found or array is NULL.
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+size_t left = 0, right = size - 1, mid;
+
+if (array == NULL)
+return (-1);
+
+while (left <= right)
+{
+printf("Searching in array: ");
+for (size_t i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i < right)
+printf(", ");
+else
+printf("\n");
+}
+
+mid = (left + right) / 2;
+
+if (array[mid] == value)
+{
+if (mid == 0 || array[mid - 1] != value)
+return (mid);
+else
+right = mid;
+}
+else if (array[mid] < value)
+{
+left = mid + 1;
+}
+else
+{
+right = mid - 1;
+}
+}
+
+return (-1);
+}
