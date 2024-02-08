62a427a7225c8f987a92845121b694ed95389e10
nenyeonyema <chinenyeonyema1@gmail.com>
2024-02-08 06:05:09 -0800

    sorting

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,6 @@
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
 #endif /* SEARCH_ALGOS_H */
